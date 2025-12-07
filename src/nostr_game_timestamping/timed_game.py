7f2d71f86e0817086ffd4e9f73b0a0349f0fb4f0
Jonathan White <jadeofsapphire@pm.me>
2025-12-06 18:05:15 -0700

    Laptop Versioning

---
+++ src/nostr_game_timestamping/timed_game.py
@@ -0,0 +1,14 @@
+from bit_timestamp import BitTimeStamp
+
+
+class TimedGameConfig:
+    """The relevant configuration for timestamp of the game.
+
+    Public methods:
+
+    Attributes:
+    - action_points
+    """
+
+    def __init__(self) -> None:
+        self.action_points = 0
